e5add9f6771577021dbe3d7f05390ea995624b49
va1shn9v <pvaishnav2718@gmail.com>
2024-04-05 16:07:16 -0400

    Init Update

---
+++ moe_inference.py
@@ -0,0 +1,32 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import warnings
+import os
+
+
+os.environ['TRANSFORMERS_CACHE'] = '/scratch/vp2499/qwen-moe/cache/'
+
+warnings.filterwarnings("ignore")
+
+
+device = "cuda" # the device to load the model onto
+model_id = "ybelkada/Mixtral-8x7B-Instruct-v0.1-bnb-4bit"
+
+model = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto")
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+prompt = "Give me a short introduction to large language model."
+
+messages = [{"role": "user", "content": prompt}]
+
+text = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+
+model_inputs = tokenizer([text], return_tensors="pt").to(device)
+
+generated_ids = model.generate(model_inputs.input_ids, max_new_tokens=512, do_sample=True)
+
+generated_ids = [output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)]
+
+response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+print("------Response-------")
+print(response)
+
